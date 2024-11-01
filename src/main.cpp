5a83668634763b8e5b6eeb2e0cf24b05abb81980
Wyatt Cade <cadewya@tamu.edu>
2024-11-01 13:41:38 -0500

    Fix repo organization, add sig handlers

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
+#include <csignal>
 #include <iostream>
 #include "detection.h"
 #include "dhexchange.h"
 
+void handler(int signum) {
+    std::remove("file");
+    exit(1);
+}
+
 int main (int argc, char *argv[]) {
+    signal(SIGTSTP, handler);
+    signal(SIGKILL, handler);
+    signal(SIGTERM, handler);
+
+    for(;;);
     if (detectVM()) {
         std::cout << "You are in a VM" << std::endl;
     } else {
         std::cout << "You are NOT in a VM" << std::endl;
     }
-    
-    manualDHExchange();
+
     return 0;
 }
